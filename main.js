3ac461b30b2bdbbeb2d797793f4cf34d578eabb2
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-10-06 12:51:14 -0600

    [UPDATE] Se termino la parte del menu de lado de desktop

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Seleccionamos el elemento del email*/
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+}
